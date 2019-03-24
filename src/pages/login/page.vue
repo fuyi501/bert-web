c541c2eafd913e2879adcac109ac4a6d928286a7
tramp <2516800961@qq.com>
2019-03-24 20:42:35 +0800

    修改logo

--- src/pages/login/page.vue
+++ src/pages/login/page.vue
@@ -23,7 +23,7 @@
           class="page-login--content-main"
           flex="dir:top main:center cross:center">
           <!-- logo -->
-          <img class="page-login--logo" src="./image/logo@2x.png">
+          <img class="page-login--logo" src="./image/logo2.png">
           <!-- 表单 -->
           <div class="page-login--form">
             <el-card shadow="never">
@@ -56,24 +56,24 @@
               <span>注册用户</span>
             </p>
             <!-- 快速登录按钮 -->
-            <el-button class="page-login--quick" size="default" type="info" @click="dialogVisible = true">
+            <!-- <el-button class="page-login--quick" size="default" type="info" @click="dialogVisible = true">
               快速选择用户（测试功能）
-            </el-button>
+            </el-button> -->
           </div>
         </div>
         <div class="page-login--content-footer">
           <p class="page-login--content-footer-options">
-            <a href="#">帮助</a>
+            <!-- <a href="#">帮助</a>
             <a href="#">隐私</a>
-            <a href="#">条款</a>
+            <a href="#">条款</a> -->
           </p>
           <p class="page-login--content-footer-copyright">
-            Copyright <d2-icon name="copyright"/> 2018 D2 Projects 开源组织出品 <a href="https://github.com/FairyEver">@FairyEver</a>
+            <!-- Copyright <d2-icon name="copyright"/> 2018 D2 Projects 开源组织出品 <a href="https://github.com/FairyEver">@FairyEver</a> -->
           </p>
         </div>
       </div>
     </div>
-    <el-dialog
+    <!-- <el-dialog
       title="快速选择用户"
       :visible.sync="dialogVisible"
       width="400px">
@@ -85,7 +85,7 @@
           </div>
         </el-col>
       </el-row>
-    </el-dialog>
+    </el-dialog> -->
   </div>
 </template>
 
@@ -118,9 +118,9 @@ export default {
       ],
       // 表单
       formLogin: {
-        username: 'admin',
-        password: 'admin',
-        code: 'v9am'
+        username: '',
+        password: '',
+        code: ''
       },
       // 校验
       rules: {
@@ -234,7 +234,7 @@ export default {
   // main
   .page-login--logo {
     width: 240px;
-    margin-bottom: 2em;
+    // margin-bottom: 2em;
     margin-top: -2em;
   }
   // 登录表单
